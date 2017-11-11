f55d461c6fe5145224d491b1bd22cd64ed52b467
frankyshtein <>
2017-11-11 20:37:44 +0300

    add some animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
+var answer;
 function large() {
-    $(this).animate({
-        width: "25%"
-    }, 400);
-    $(this).on('click', function () {
-        $(this).addClass("flip");
-        $(this).fadeOut(500,function(){
-        });
-    })
+    $(".module img").remove();
+    $(".module").on('click',small);
+    answer = $(this).index()+2;
+//    answer = $(".wrapper img:nth-child("+a+")");
+    $(this).clone().appendTo($(".module"));
+    $(".module").fadeIn(400,function() {
+        $(".module img").on('click',flip);
+    });
 }
-$("img").on('click', large);
+
+function small(e) {
+var target = $(e.target);
+  if(!target.parents().hasClass('module')){
+        $(".module").fadeOut(400);
+  }
+}
+function flip() {
+    $(this).addClass("flip");
+    setTimeout(function(){
+        $(".module img").remove();
+        console.log(1)
+        $(".wrapper img:nth-child("+answer+")").clone().appendTo($(".module"));
+        $(".module img").removeClass("hide");
+    },1000);
+}
+$("body img").on('click', large);
